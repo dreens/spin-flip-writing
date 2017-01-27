a09d44dbb14c3a0158e02ab54fa9e3e4f52f0af2
David Reens <dave.reens@gmail.com>
2017-01-26 23:06:14 -0700

    Figure Messing
    Pink Laser, better colors on loss surfaces, legend.

--- loss_surfaces_convert.m
+++ loss_surfaces_convert.m
@@ -8,6 +8,10 @@ red   = image(:,:,1);
 green = image(:,:,2);
 blue  = image(:,:,3);
 
+red2   = image(:,:,1);
+green2 = image(:,:,2);
+blue2  = image(:,:,3);
+
 
 imtool(image)
 
@@ -22,53 +26,53 @@ getplane = @(r1,r2,g1,g2,b1,b2) ...
 
 %red
 plane = getplane(110,210,-1,80,-1,60);
-red(plane) = 120;
-green(plane) = 0;
-blue(plane) = 0;
+red2(plane) = 120;
+green2(plane) = 0;
+blue2(plane) = 0;
 
 %orange
 plane = getplane(200,256,81,163,-1,105);
-red(plane) = 180;
-green(plane) = 0;
-blue(plane) = 0;
+red2(plane) = 180;
+green2(plane) = 0;
+blue2(plane) = 0;
 
 %yellow
-plane = getplane(180,256,162,202,-1,43);
-red(plane) = 150;
-green(plane) = 150;
-blue(plane) = 0;
+plane = getplane(180,256,162,202,-1,63);
+red2(plane) = 150;
+green2(plane) = 150;
+blue2(plane) = 0;
 
 %green
 plane = getplane(52,152,151,195,6,186);
-red(plane) = 0;
-green(plane) = 0;
-blue(plane) = 140;
+red2(plane) = 135;
+green2(plane) = 135;
+blue2(plane) = 135;
 
 %light blue
 plane = getplane(-1,80,100,190,190,256);
-red(plane) = 0;
-green(plane) = 135;
-blue(plane) = 255;
+red2(plane) = 0;
+green2(plane) = 0;
+blue2(plane) = 140;
 
 %dark blue
-plane = getplane(-1,35,-1,101,-1,191);
-red(plane) = 65;
-green(plane) = 115;
-blue(plane) = 205;
+plane = getplane(-1,35,-1,101,-1,161);
+red2(plane) = 65;
+green2(plane) = 115;
+blue2(plane) = 205;
 
 %background
 plane = getplane(200,256,200,256,200,256);
-red(plane) = 255;
-green(plane) = 255;
-blue(plane) = 255;
+red2(plane) = 255;
+green2(plane) = 255;
+blue2(plane) = 255;
 
 
 
 
 image2 = image;
-image2(:,:,1) = red;
-image2(:,:,2) = green;
-image2(:,:,3) = blue;
+image2(:,:,1) = red2;
+image2(:,:,2) = green2;
+image2(:,:,3) = blue2;
 
 %some clipping
 image2 = image2(250:1450,:,:);
